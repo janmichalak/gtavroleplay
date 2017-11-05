1389c4716f5ee7dbb11e121cbb5911864d70781a
janmichalak <vicebon@gmail.com>
2017-11-06 00:34:55 +0100

    naprawione itemy, dodany config Mods

---
+++ Vehicles/Mods.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lsrp_gamemode.Vehicles
+{
+    class Mods
+    {
+        public static readonly int SPOILERS = 0;
+        public static readonly int FRONT_BUMPER = 1;
+        public static readonly int REAR_BUMPER = 2;
+        public static readonly int SIDE_SKIRT = 3;
+        public static readonly int EXHAUST = 4;
+        public static readonly int FRAME = 5;
+        public static readonly int GRILLE = 6;
+        public static readonly int HOOD = 7;
+        public static readonly int FENDER = 8;
+        public static readonly int RIGHT_FENDER = 9;
+        public static readonly int ROOF = 10;
+        public static readonly int ENGINE = 11;
+        public static readonly int BRAKES = 12;
+        public static readonly int TRANSMISSION = 13;
+        public static readonly int HORNS = 14;
+        public static readonly int SUSPENSION = 15;
+        public static readonly int ARMOR = 16;
+        public static readonly int TURBO = 18;
+        public static readonly int XENON = 22;
+        public static readonly int FRONT_WHEELS = 23;
+        public static readonly int BACK_WHEELS = 24;
+        public static readonly int PLATE_HOLDERS = 25;
+        public static readonly int TRIM_DESIGN = 27;
+        public static readonly int ORNAMENTS = 28;
+        public static readonly int DIAL_DESIGN = 30;
+        public static readonly int STEERING_WHEEL = 33;
+        public static readonly int SHIFT_LEVER = 34;
+        public static readonly int PLAQUES = 35;
+        public static readonly int HYDRAULICS = 38;
+        public static readonly int LIVERY = 48;
+        public static readonly int PLATE = 62;
+        public static readonly int COLOUR_1 = 66; //(0 to 74)
+        public static readonly int COLOUR_2 = 67; //(0 to 74)
+        public static readonly int WINDOW_TINT = 69;
+        public static readonly int DASHBOARD_COLOR = 74;
+        public static readonly int TRIM_COLOR = 75;
+    }
+}
